73c3fc1b8df20303c17c96bfb3677ac471c39f61
eepseelona <mayer.hedda@gmail.com>
2023-06-12 10:11:56 +0200

    sidebar

--- webshop/js/script.js
+++ webshop/js/script.js
@@ -3,25 +3,36 @@ var sidebar = document.getElementById('sidebar');
 var button = document.getElementById('sideButton');
 var content = document.getElementById('content');
 var localSidebar = localStorage.getItem('sidebar');
+var degree = 0;
 
-if(!localSidebar || localSidebar == true) {
+if(!localSidebar || localSidebar == "true") {
     sidebar.style.width = "300px";
     content.style.marginLeft = "340px";
+
+    localStorage.setItem("sidebar", "true");
+    localSidebar = localStorage.getItem("sidebar");
 } else {
     sidebar.style.width = "21px";
     content.style.marginLeft = "41px";
+
+    localSidebar = localStorage.getItem("sidebar");
 }
 
 function sideHidden() {
-    if(localSidebar == true) {
+    console.log(localSidebar)
+    if(localSidebar == "true") {
         sidebar.style.width = "21px";
         content.style.marginLeft = "41px";
 
-        localStorage.setItem("sidebar", false);
+        localStorage.setItem("sidebar", "false");
+        localSidebar = localStorage.getItem('sidebar');
     } else {
         sidebar.style.width = "300px";
         content.style.marginLeft = "340px";
 
-        localStorage.setItem("sidebar", true);
+        localStorage.setItem("sidebar", "true");
+        localSidebar = localStorage.getItem('sidebar');
     }
+    degree += 180;
+    button.style.transform = `rotate(${degree}deg)`;
 }
